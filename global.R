caf278cc5a6022f88ac8f3ecffd99b28a5bae2ff
dumontjo <joshua.dumont@edu.esiee.fr>
2023-10-17 15:42:53 +0200

    nouveau ajout histogramme

--- global.R
+++ global.R
@@ -4,6 +4,7 @@ library(shiny)
 library(gapminder)
 library(dplyr)
 library(ggplot2)
+library(plotly)
 
 # On importe nos données
 disaster_data <- read_csv("./natural_disaster.csv")
--- server.R
+++ server.R
@@ -6,7 +6,8 @@ server <- function(input, output) {
     disaster_data %>%
       ggplot(aes(x = `Total Deaths`)) + 
       geom_histogram(binwidth = 0.1, fill = "blue", color = "black", alpha = 0.7) + 
-      scale_x_log10(labels = scales::comma) +
+      scale_x_log10(breaks = c(1, 10, 100, 1000, 10000, 100000, 1000000), 
+                    labels = scales::comma) +
       scale_y_log10(labels = scales::comma) +
       theme_minimal() +
       labs(title = "Histogramme des décès totaux dus aux catastrophes naturelles (Échelles logarithmiques)",
