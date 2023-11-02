f1157f692e553f6d9e7bac2fbf776d4d6f871ae8
dumontjo <joshua.dumont@edu.esiee.fr>
2023-11-02 16:55:32 +0100

    Ajout de la carte

--- global.R
+++ global.R
@@ -1,10 +1,47 @@
 # global.R
-library(tidyverse)
+
+# Chargement des bibliothèques
 library(shiny)
-library(gapminder)
+library(leaflet)
 library(dplyr)
-library(ggplot2)
+library(readr)
 library(plotly)
+library(tidyverse)
+library(ggplot2)
+library(gapminder)
+
+# Chargement et préparation des données du premier fichier
+
+df_catastrophe <- read.csv("natural_disaster.csv",sep=",")
+df_geoloc <- read.csv("new_dataframe.csv",sep=",")
+
+df_catastrophe$Latitude <- df_geoloc$Latitude
+df_catastrophe$Longitude <- df_geoloc$Longitude
+
+df_catastrophe_location <- df_catastrophe %>% filter(!is.na(Latitude) & !is.na(Longitude))
+
+annees <- unique(df_catastrophe_location$Year)
+
+marqueur_type_de_catastrophe <- list(
+  "Drought" = list(color = 'beige', icon = 'tint'),
+  "Drouuake" = list(color = 'beige', icon = 'tint'),
+  "Volcanic activity" = list(color = 'orange', icon = 'fire'),
+  "Mass movement (dry)" = list(color = 'black', icon = 'warning-sign'),
+  "Storm" = list(color = 'darkblue', icon = 'cloud'),
+  "Earthquake" = list(color = 'purple', icon = 'exclamation-sign'),
+  "Earthquakeght" = list(color = 'purple', icon = 'exclamation-sign'),
+  "Earthq" = list(color = 'purple', icon = 'exclamation-sign'),
+  "Flood" = list(color = 'blue', icon = 'tint'),
+  "Epidemic" = list(color = 'pink', icon = 'plus-sign'),
+  "Landslide" = list(color = 'darkgreen', icon = 'arrow-down'),
+  "Wildfire" = list(color = 'red', icon = 'fire'),
+  "Extreme temperature" = list(color = 'darkred', icon = 'warning-sign'),
+  "Fog" = list(color = 'lightgray', icon = 'cloud'),
+  "Insect infestation" = list(color = 'green', icon = 'leaf'),
+  "Impact" = list(color = 'black', icon = 'asterisk'),
+  "Animal accident" = list(color = 'green', icon = 'leaf'),
+  "Glacial lake outburst" = list(color = 'lightblue', icon = 'tint')
+)
 
 # On importe nos données
 disaster_data <- read_csv("./natural_disaster.csv")
--- ui.R
+++ ui.R
@@ -1,30 +1,28 @@
 # ui.R
 
 fluidPage(
-  # Titre
-  titlePanel("Histogramme des décès dus aux catastrophes naturelles"),
+  tags$head(tags$link(rel="stylesheet", type="text/css", href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css")),
   
-  # RangeSlider pour sélectionner la plage d'années
-  sliderInput("year_slider", 
-              "Sélectionnez une plage d'années:", 
-              min = min(disaster_data$Year),
-              max = max(disaster_data$Year),
-              value = c(min(disaster_data$Year), max(disaster_data$Year)),
-              step = 1,
-              round = TRUE,
-              sep = "",
-              width = "100%"),
+  titlePanel("Mon Dashboard"),
   
-  # Histogrammes
-  plotOutput("graph1"),
-  plotOutput("graph2"),
-  plotOutput("graph3"),
-  plotlyOutput("graph4"),
-  
-  
-  # Description
-  div(children = list(
-    "Les histogrammes représentent la distribution du nombre total de décès dus aux catastrophes naturelles."
-  ))
-  
+  tabsetPanel(
+    tabPanel("Histogrammes",
+             # Éléments de l'interface utilisateur de ui.R
+             sliderInput("year_slider", "Sélectionnez une plage d'années:", min = 1900, max = 2021, value = c(1900, 2021),step = 1,round = TRUE,sep = "",width = "100%"),
+             plotOutput("graph1"),
+             plotOutput("graph2"),
+             plotOutput("graph3"),
+             plotlyOutput("graph4")
+    ),
+    tabPanel("Carte des catastrophes",
+             # Éléments de l'interface utilisateur de ui_map.R
+             fluidRow(
+               column(12, 
+                      sliderInput("range", "Sélectionnez une plage d'années:", min = min(annees), max = max(annees), value = c(max(annees)-1, max(annees)),step = 1,round = TRUE,sep = "",width = "100%")
+               ),
+               # ... (autres éléments de l'interface utilisateur pour la carte)
+             ),
+             leafletOutput("map")
+    )
+  )
 )
